31c95151d1c06dc4805190f3f957d9e06cd39b3d
o3ouo <o3ouo@daum.net>
2024-11-15 15:33:42 +0900

    js..

---
+++ 241115/js/script_04.js
@@ -0,0 +1,19 @@
+
+let item = document.querySelectorAll(".item")
+console.log(item)
+
+item.forEach( (items, index, array) => {
+  console.log(array[index]);
+
+  let text = items.querySelector(".text");
+
+  array[index].addEventListener("mouseover", () => {
+    text.style.display = "block";
+  });
+
+  array[index].addEventListener("mouseleave", () => {
+    text.style.display = "none";
+  });
+
+});
+
---
+++ 241115/js/script_05.js
@@ -0,0 +1,13 @@
+let btn = document.querySelectorAll(".right .img");
+
+
+btn.forEach( (btns, i, a) => {
+  
+
+  a[i].addEventListener("click", () => {
+  let img = document.querySelector(".left .img");
+    
+    img.setAttribute('src', `../event_test/p${i}.jpg`);
+  });
+  
+});
